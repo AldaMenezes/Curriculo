afb6edae584637ca57c674a26f89baf935e99d7e
Alda Menezes <aldinhamenezes@gmail.com>
2022-04-02 12:24:33 -0300

    enviando projeto

---
+++ funcao.js
@@ -0,0 +1,16 @@
+ // Criando uma interação com o usuario
+ var nome;
+ nome = prompt("Olá seja bem vindo qual é o seu nome ?");
+ //alert("Prazer em telo voçê aqui " +nome);
+
+
+
+ // Criando uma função em JS
+ function mensagem() {
+     alert("Olá um suuuper bom dia!!!!");
+
+
+ }
+ function vindas() {
+     alert("Seja bem vindo "  +nome);
+ }
